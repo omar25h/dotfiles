edfe46f6080d653f5ddc6613fd42c931f62d0728
Omar El Halabi <omar_halabi25@hotmail.com>
2020-12-24 07:09:40 +0200

    Move to init.lua only
    Neovim updated the editor to load configuration from either
    init.vim or init.lua. It would be an error to have both.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,66 +1,191 @@
+-------------------------------------------------------------------------------------------------
+-- Plugins
+-------------------------------------------------------------------------------------------------
+
+require'packer'.startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-treesitter/completion-treesitter'
+
+  use 'sheerun/vim-polyglot'
+
+  use 'editorconfig/editorconfig-vim'
+
+  use 'airblade/vim-gitgutter'
+  use 'tpope/vim-fugitive'
+
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+
+  use 'mileszs/ack.vim'
+
+  use 'tpope/vim-surround'
+  use 'tpope/vim-commentary'
+
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+  use 'morhetz/gruvbox'
+end)
+
+-------------------------------------------------------------------------------------------------
+-- General Configuration
+-------------------------------------------------------------------------------------------------
+
+vim.o.autoread = true
+vim.o.autowrite = true
+vim.o.mouse = 'a'
+vim.o.path = vim.o.path .. ',**'
+vim.o.wildmenu = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.hlsearch = false
+vim.o.termguicolors = true
+vim.o.smartcase = true
+vim.o.completeopt = 'menuone,noinsert,noselect'
+vim.o.omnifunc = 'v:lua.vim.lsp.omnifunc'
+vim.o.shortmess = vim.o.shortmess .. 'c'
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes:1'
+
+vim.cmd([[syntax on]])
+if vim.wo.diff then
+  vim.cmd([[syntax off]])
+end
+
+vim.g.mapleader = ','
+vim.g.filetype_m = 'objc'
+
+-------------------------------------------------------------------------------------------------
+-- Styling
+-------------------------------------------------------------------------------------------------
+
+vim.api.nvim_exec ([==[
+colorscheme gruvbox
+highlight LineNr ctermfg=1 guifg=#6F6F6F ctermbg=NONE guibg=NONE
+highlight SignColumn ctermbg=NONE guibg=NONE
+highlight CursorLineNr ctermbg=NONE guibg=NONE
+
+highlight GitGutterAddDefault guibg=NONE
+highlight GitGutterChangeDefault guibg=NONE
+highlight GitGutterDeleteDefault guibg=NONE
+]==], false)
+
+-------------------------------------------------------------------------------------------------
+-- Airline
+-------------------------------------------------------------------------------------------------
+
+vim.g.airline_powerline_fonts=1
+vim.g.airline_theme='base16_gruvbox_dark_hard'
+
+-------------------------------------------------------------------------------------------------
+-- Ack
+-------------------------------------------------------------------------------------------------
+
+if vim.fn.executable('rg') then
+  vim.g.ackprg='rg --vimgrep'
+end
+
+-------------------------------------------------------------------------------------------------
+-- Keymap
+-------------------------------------------------------------------------------------------------
+
+-- General
+vim.api.nvim_set_keymap('', '<C-n>', '<Cmd>cnext<CR>', {})
+vim.api.nvim_set_keymap('', '<C-p>', '<Cmd>cprevious<CR>', {})
+vim.api.nvim_set_keymap('n', '<Leader>a', '<Cmd>cclose<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>ne', '<Cmd>LuaTreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-S-T>', '<Cmd>vs<bar>:b#<CR>', {})
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
+
+-- LSP
+vim.api.nvim_set_keymap('n', '<C-]>', '<Cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '1gD', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'g0', '<Cmd>lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gW', '<Cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.declaration()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>fmt', '<Cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>dl', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', { noremap = true, silent = true })
+
+-- Popup Menu
+vim.api.nvim_set_keymap('i', '<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { noremap = true, expr = true })
+vim.api.nvim_set_keymap('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { noremap = true, expr = true })
+
+-------------------------------------------------------------------------------------------------
+-- Gitgutter
+-------------------------------------------------------------------------------------------------
+
+vim.api.nvim_set_keymap('n', ']h', '<Plug>(GitGutterNextHunk)', {})
+vim.api.nvim_set_keymap('n', '[h', '<Plug>(GitGutterPrevHunk)', {})
+
+-------------------------------------------------------------------------------------------------
+-- LSP
+-------------------------------------------------------------------------------------------------
+
 local lspconfig = require'lspconfig'
 local completion = require'completion'
 
-local root_pattern = lspconfig.util.root_pattern
-
--------------------------------------------------------------------------------------------------
 -- Rust
--------------------------------------------------------------------------------------------------
-
 lspconfig.rust_analyzer.setup{
   on_attach = completion.on_attach,
 }
 
--------------------------------------------------------------------------------------------------
 -- C/C++/Objective-C
--------------------------------------------------------------------------------------------------
-
 lspconfig.ccls.setup{
   on_attach = completion.on_attach,
 }
 
--------------------------------------------------------------------------------------------------
 -- Golang
--------------------------------------------------------------------------------------------------
-
 lspconfig.gopls.setup{
   on_attach = completion.on_attach,
 }
 
--------------------------------------------------------------------------------------------------
 -- JavaScript/TypeScript
--------------------------------------------------------------------------------------------------
-
 lspconfig.tsserver.setup{
   on_attach = completion.on_attach,
 }
 
--------------------------------------------------------------------------------------------------
 -- JSON
--------------------------------------------------------------------------------------------------
-
 lspconfig.jsonls.setup{
   on_attach = completion.on_attach,
 }
 
--------------------------------------------------------------------------------------------------
 -- Scala
--------------------------------------------------------------------------------------------------
-
 lspconfig.metals.setup{
   on_attach = completion.on_attach,
 }
 
--------------------------------------------------------------------------------------------------
--- LaTex
--------------------------------------------------------------------------------------------------
+-- Lua
+lspconfig.sumneko_lua.setup {
+  on_attach = completion.on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        disable = {
+          "lowercase-global",
+          "undefined-global",
+        },
+      },
+    },
+  },
+}
 
+-- LaTex
 lspconfig.texlab.setup{
   on_attach = completion.on_attach,
   settings = {
     latex = {
       build = {
-        args = { 
+        args = {
           "-pdf",
           "-interaction=nonstopmode",
           "-synctex=1",
@@ -82,3 +207,4 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
   },
 }
+
