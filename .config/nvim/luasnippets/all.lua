8660e9d0740db5382faa8d7b8e4faec0e5da9a58
Omar El Halabi <omar_halabi25@hotmail.com>
2023-06-30 09:05:34 +0200

    fix(nvim): handle comment strings with 2 parts for todo snippets

--- .config/nvim/luasnippets/all.lua
+++ .config/nvim/luasnippets/all.lua
@@ -1,38 +1,48 @@
 ---@diagnostic disable: undefined-global
 
-local function with_cmt(cmt)
-  return string.format(vim.bo.commentstring, cmt)
+local function comment_parts()
+  local commentstring = vim.bo.commentstring .. '%s'
+  local parts = {}
+  for w in commentstring:gmatch '(.-)%%s' do
+    table.insert(parts, w)
+  end
+  return parts
+end
+
+local function create_special_comment(name)
+  local parts = comment_parts()
+
+  local nodes = {
+    t(parts[1] .. name),
+    c(1, {
+      sn(1, { t '(', i(1, 'OH'), t ')' }),
+      t '',
+    }),
+    t ': ',
+    i(2),
+  }
+
+  if #parts == 2 then
+    table.insert(nodes, t(parts[2]))
+  end
+
+  return nodes
 end
 
 return {
   s({ trig = 'fixme', name = 'FIXME' }, {
     d(1, function()
-      return s('', {
-        f(function()
-          return with_cmt 'FIXME'
-        end, {}),
-        c(1, {
-          sn(1, { t '(', i(1, 'OH'), t ')' }),
-          t '',
-        }),
-        t ': ',
-        i(0),
-      })
+      return s('', create_special_comment 'FIXME')
     end),
   }),
   s({ trig = 'todo', name = 'TODO' }, {
     d(1, function()
-      return s('', {
-        f(function()
-          return with_cmt 'TODO'
-        end, {}),
-        c(1, {
-          sn(1, { t '(', i(1, 'OH'), t ')' }),
-          t '',
-        }),
-        t ': ',
-        i(0),
-      })
+      return s('', create_special_comment 'TODO')
+    end),
+  }),
+  s({ trig = 'note', name = 'NOTE' }, {
+    d(1, function()
+      return s('', create_special_comment 'NOTE')
     end),
   }),
 }, {}
