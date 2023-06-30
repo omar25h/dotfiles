dfd15da5f0a5e3a587bbdfd96cbaa2c9af43f57a
Omar El Halabi <omar_halabi25@hotmail.com>
2023-06-30 18:12:54 +0200

    feat(nvim): disable more builtin plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,6 @@
 vim.g.mapleader = ','
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
+require 'oh.disabled_plugins'
 
 vim.loader.enable()
 
