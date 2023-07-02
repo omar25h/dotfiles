7cc15a546b19f03c50618b0bc0c1bce7e52149c3
Omar El Halabi <omar_halabi25@hotmail.com>
2023-07-02 23:28:24 +0200

    feat(nvim): drop unused dependency

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -4,7 +4,6 @@ return {
     dependencies = {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-nvim-lsp',
       'onsails/lspkind-nvim',
       'saadparwaiz1/cmp_luasnip',
