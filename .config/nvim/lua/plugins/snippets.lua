aacd18c7e51e2ea273858c6d60fc2773d0156687
Omar El Halabi <omar_halabi25@hotmail.com>
2023-11-01 10:57:59 +0100

    chore(nvim): use latest version of LuaSnip

--- .config/nvim/lua/plugins/snippets.lua
+++ .config/nvim/lua/plugins/snippets.lua
@@ -2,7 +2,6 @@ return {
   {
     'L3MON4D3/LuaSnip',
     event = 'InsertEnter',
-    version = '2.*',
     build = 'make install_jsregexp',
     config = function() require('oh.snippets').setup() end,
   },
